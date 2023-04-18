05fe0c3aa8301a13dd0cb3b903e1524f59716f07
takumi <c0a2212292@edu.teu.ac.jp>
2023-04-18 16:53:32 +0900

    練習５

--- pygame_image.py
+++ pygame_image.py
@@ -6,6 +6,7 @@ def main():
     screen = pg.display.set_mode((800, 600))
     clock  = pg.time.Clock()
     bg_img = pg.image.load("ex01/fig/pg_bg.jpg")
+    bg_imgs = [bg_img,pg.transform.flip(pg.image.load("ex01/fig/pg_bg.jpg"),True,False)]
     kk_img = pg.image.load("ex01/fig/3.png")
     kk_img = pg.transform.flip(pg.image.load("ex01/fig/3.png"),True,False)
     kk_imgs = [kk_img,pg.transform.rotozoom(kk_img,10, 1.0)]
@@ -20,11 +21,12 @@ def main():
 
         tmr += 1
 
-        if x>= 1599:
+        if x>= 3199:
             x=0
         x += 1
-        screen.blit(bg_img, [-x, 0])
-        screen.blit(bg_img, [1600-x, 0])
+        screen.blit(bg_imgs[0], [-x, 0])
+        screen.blit(bg_imgs[1], [1600-x, 0])
+        screen.blit(bg_imgs[0],[3200-x,0])
         if y>=100:
             y=0
         y += 1
